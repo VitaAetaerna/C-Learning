5827676e4aca98b54de27cb9b9e824f1844c486b
Leon² <75674735+VitaAetaerna@users.noreply.github.com>
2024-01-17 10:29:54 +0100

    Create DataTypes.c

---
+++ DataTypes.c
@@ -0,0 +1,17 @@
+#include "stdio.h"
+
+int main(){
+    int IntA = 0;
+    int IntB = 0;
+
+    char CharA = NULL;
+    char CharB = NULL;
+
+    float FloatA = 1.2f;
+    float FloatB = 2.75f;
+
+
+    
+
+    return 0;
+}
