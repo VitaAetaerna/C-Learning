2100c1f735551b1c5e4c94997dd014350918a0c4
Leon² <75674735+VitaAetaerna@users.noreply.github.com>
2024-01-17 13:16:07 +0100

    Create Fibonacci.c

---
+++ Fibonacci.c
@@ -0,0 +1,19 @@
+#include "stdio.h"
+
+int main(){
+    int term1 = 0;
+    int term2 = 1;
+    int i,n = 0;
+
+    int NextTerm = term1 + term2;
+
+    scanf("Enter Number of terms: %d \n", &n);
+    
+    for (i=3; i <= n; i++){
+        printf("%d", NextTerm);
+        term1 = term2;
+        term2 = NextTerm;
+        NextTerm = term1 + term2;
+    }
+    return 0;
+}
