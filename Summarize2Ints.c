90a93875262211dc1dab0a2733bd18ea02e204d4
Leon² <75674735+VitaAetaerna@users.noreply.github.com>
2024-01-17 10:10:48 +0100

    Create Summarize2Ints.c

---
+++ Summarize2Ints.c
@@ -0,0 +1,14 @@
+#include "stdio.h"
+
+int main(){
+    int A = 0;
+    int B = 0;
+    //Get Input
+    printf("Gebe deine Zahl ein: ");
+    scanf("%d", &A);
+    printf("\n Gebe die Zweite Zahl ein: ");
+    scanf("%d", &B);
+    // Errechne A+B
+    printf("%i", A+B);
+    return 0;
+};
