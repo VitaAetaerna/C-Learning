0a1ef8c8b005055d1ac26be7f6ae43462e609d2f
Leon² <75674735+VitaAetaerna@users.noreply.github.com>
2024-01-17 11:36:54 +0100

    Create Calculator.c

---
+++ Calculator.c
@@ -0,0 +1,19 @@
+#include "stdio.h"
+
+int main(){
+    int Num1 = 0;
+    int Num2 = 0;
+    char Operator;
+    printf("Enter your Calculation (1 + 1), (5 - 4) or smth\n");
+    scanf("%i %c %i \n", &Num1, &Operator, &Num2);
+
+    switch(Operator){
+        case '+': 
+            printf("%i \n", Num1+Num2);
+            break;
+        case '-' :
+            printf("%i \n", Num1-Num2);
+            break;
+    }
+    return 0;
+}
