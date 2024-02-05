7ce4c0c733133cc73b8ca9c1543958ec1aa97b83
Leon² <75674735+VitaAetaerna@users.noreply.github.com>
2024-02-05 09:08:48 +0100

    Update BinaryCalculator.c

--- BinaryCalculator.c
+++ BinaryCalculator.c
@@ -1,1 +1,25 @@
+#include "stdio.h"
 
+int BinaryCalc(int DezZahl){
+    int binaryNumber[8];
+    printf("%i", sizeof(binaryNumber));
+    for (int i=0; i<8; i++){
+        binaryNumber[i] = DezZahl % 2;
+        DezZahl = DezZahl / 2;
+        printf("Die Stelle %i der Zahl %i ist %d \n", i,DezZahl,binaryNumber[i]);
+    }
+
+    return 0;
+    
+}
+
+int main(){
+    int DezZahl = 0;
+
+    printf("Gib eine Zahl ein an: ");
+    scanf("%i", &DezZahl);
+
+    BinaryCalc(DezZahl);
+    return 0;
+
+}
